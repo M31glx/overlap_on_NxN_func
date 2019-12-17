26460641d28aa98908a25d5a0c0d99f63f57efe6
faskowit <jfaskowi@iu.edu>
2019-12-17 17:55:17 -0500

    workin

--- src/analysis/overlap_metrics_across_alg.m
+++ src/analysis/overlap_metrics_across_alg.m
@@ -9,12 +9,18 @@ run(config_file) ;
 
 %% load up thresholded network 
 
+cmapjet = jet(7);
+cmapjet(4,:) = 1;
+cmapjet = interp1(linspace(0,1,size(cmapjet,1)),cmapjet,linspace(0,1,256));
+
 thresholds = [ 0.1 0.2 0.3 0.4 ] ;
 loadDat = struct() ;
 
 % methodstr = { 'nmf' 'svinet' 'agmfit' 'thrlinkbin' 'thrlink' } ;
 % 'thrlinkbin' is effectively the same as 'thrlink' method
 methodstr = { 'nmf' 'svinet' 'agmfit' 'thrlink' } ;
+figmethodstr = { 'NMF' 'SVINET' 'AGMFIT' 'ThrLink' } ;
+
 
 for idx = 1:length(thresholds) 
 
@@ -99,15 +105,16 @@ efc_numc = median(loadEnt.tot,2) ;
 
 %%
 
-plotit = true ;
+plotit = false ;
 
 capitl = @(x) [upper(x(1)) x(2:end)] ;
 
 % function [ha, pos] = tight_subplot(Nh, Nw, gap, marg_h, marg_w)
 if ~plotit
-set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 0.4 0.6]);      
-ts = tight_subplot(1,length(thresholds)) ;
+set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 0.5 0.6]);      
+ts = tight_subplot(1,length(thresholds),0.001) ;
 end
+
 
 for idx = 1:length(thresholds)
 
@@ -119,19 +126,30 @@ for idx = 1:length(thresholds)
         ovrRes(idx).thrlink.avg_ent ...
         ovrRes(idx).agmfit.avg_ent ...
         ovrRes(idx).svinet.avg_ent ...
-        ovrRes(idx).nmf.avg_ent]) ;
+        ovrRes(idx).nmf.avg_ent],...
+        'type','spearman') ;
     imsc = imagesc(methodsCorr) ;
-
+    %imsc = heatmap(methodsCorr)
+    colormap()
+    
     set(gca,'YTickLabel',[]);
-    set(gca,'XTickLabel', [ 'eFC' fliplr(cellfun(capitl,methodstr,'UniformOutput',false))])
+    set(gca,'XTickLabel', [ 'eFC' fliplr(cellfun(capitl,figmethodstr,'UniformOutput',false))])
     xtickangle(45)
-    colorbar()
+    ccb = colorbar()
+    if idx == 4
+        ylabel(ccb,'Correlation')
+    else
+        ccb.Visible = 'off' ;
+        
+    end
     caxis([0 1])
-    title(['Threshold ' num2str(thresholds(idx))])
+    title(['Density ' num2str(thresholds(idx))])
     
     set(gca,'ytick',[])
     axis square
 
+    set(gca,'FontSize',10)
+    
     if plotit
         print(gcf,...
         '-dpdf',[ PDF_FIG_DIR '/thr_' num2str(thresholds(idx)) '_similaritymat.pdf' ]);
@@ -140,9 +158,17 @@ for idx = 1:length(thresholds)
     
 end
 
+if ~plotit
+    orient(gcf,'landscape')
+
+        print(gcf,...
+        '-dpdf',[ PDF_FIG_DIR '/allthr_similaritymat.pdf' ],'-fillpage');
+        close(gcf);
+end
+        
 %%
 
-plotit = true ;
+plotit = false ;
 
 capitl = @(x) [upper(x(1)) x(2:end)] ;
 
@@ -151,7 +177,7 @@ rng(123)
 if ~plotit
 % function [ha, pos] = tight_subplot(Nh, Nw, gap, marg_h, marg_w)
 set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 0.5 0.5]);      
-ts = tight_subplot(1,length(thresholds)) ;
+ts = tight_subplot(length(thresholds),length(thresholds)) ;
 end
 
 
@@ -161,7 +187,9 @@ for idx = fliplr(1:4)
     thridx = kdx ;
     
     if ~plotit
-    axes(ts(5-idx))
+        aa= (5-idx)
+        aa = aa + 4 *(kdx-1)
+    axes(ts(aa))
     end
     
     scatcolros = loadCmap.cmap(loadAff.lab) ;
@@ -202,12 +230,14 @@ for idx = fliplr(1:4)
     annotText = [ '$\rho = ' num2str(round(cc,2)) '$' ] ;
     % '[' num2str(round(ci(1),2)) ',' num2str(round(ci(2),2)) ']'
     xr = xlim ; yr = ylim ;
-    text(xr(2)*0.7,.1,annotText,'fontsize',14,'Interpreter','latex')
+    text(xr(2)*0.5,.1,annotText,'fontsize',14,'Interpreter','latex')
 
     if plotit
         print(gcf,...
         '-dpdf',[ PDF_FIG_DIR '/thr_' num2str(thresholds(kdx)) '_scatter_efc_vs_' methodstr{idx} '.pdf' ]);
         close(gcf);
+    else
+        disp('wowowow')
     end
 
 end
@@ -269,5 +299,110 @@ end
 
 save([OUTDIR_PROC '/overlap_norment' PARC_STR '.mat'],...
     'overlap_norment', 'thresholds','-v7.3')
+
+%% 
+
+% overlap_mutinfo = struct() ;
+
+% get some indexing
+n = NUM_NODES;
+[ii,iii] = find(triu(ones(n),1));
+einds = [ii,iii] ;
+
+efcomms = loadCons.Ci(:,10,1) ;
+h = zeros(NUM_NODES,10) ;
+for i = 1:NUM_NODES
+    idx = einds(:,1) == i | einds(:,2) == i;
+    h(i,:) = hist(efcomms(idx),1:10);
+end
+hbin = h>0 ; 
+
+for idx = 1 %:length(thresholds)
+
+    sss = cell(5,1) ;
+    sss{1} = hbin ;
+    sss{2} = loadDat(idx).nmf.nmf_cent ;
+    sss{3} = loadDat(idx).svinet.svinet_cent ;
+    sss{4} = loadDat(idx).agmfit.agmfit_cent ;
+    sss{5} = loadDat(idx).thrlink.thrlink_cent ;
+   
+    rng(123)
+
+    bootSamp = 10000 ;
+    bootRes = zeros(5,bootSamp);
+
+    for bdx = 1:bootSamp
+        disp(bdx)
+        
+        randinds = randsample(200,200,'false') ;
+        
+        for jdx = 1:5
+           bootRes(jdx,bdx) = gnmi(sss{1},sss{jdx}(randinds),NUM_NODES) ;
+        end
+    
+    end
+end
+
+%% do some quick stata
+
+dat = overlap_norment.eFC ; 
+groupping = ones(length(dat),1) ;
+
+methodstr = { 'nmf' 'svinet' 'agmfit' 'thrlink' } ;
+
+for idx = 1:length(methodstr)
+    aa = overlap_norment(1).(methodstr{idx}) ;
+    dat = [ dat ; aa ];
+    groupping = [ groupping ; (ones(length(aa),1) .* (idx+1)) ] ;
+end
+
+[P,A,S] = anova1(dat,groupping);
+
+%% bootstap correlations
+% I don't really really like the test... i don't think it perfectly...
+
+rng(123)
+
+bootSamp = 10000 ;
+bootRes = zeros(5,5,bootSamp);
+
+bootRes2 = zeros(3,bootSamp) ;
+
+for idx = 1 %:length(thresholds)
+
+    normaldat = [efc_ent ...
+        ovrRes(idx).thrlink.avg_ent ...
+        ovrRes(idx).agmfit.avg_ent ...
+        ovrRes(idx).svinet.avg_ent ...
+        ovrRes(idx).nmf.avg_ent] ;
+    [a,b] = corr(normaldat,...
+        'type','spearman') ;
+    
+for jdx = 1:bootSamp
+    disp(jdx)
+    
+    randinds = randsample(200,200,'true') ;
+    
+    resampdat = [efc_ent(randinds) ...
+        ovrRes(idx).thrlink.avg_ent(randinds) ...
+        ovrRes(idx).agmfit.avg_ent(randinds) ...
+        ovrRes(idx).svinet.avg_ent(randinds) ...
+        ovrRes(idx).nmf.avg_ent(randinds)] ;
+    
+    ccc = corr(resampdat,...
+        'type','spearman') ;
+    
+    bootRes(:,:,jdx) = ccc ; 
+
+    bootRes2(1,jdx) = ccc(2,1) ;
+    bootRes2(2,jdx) = max(ccc(3:5,1));
+    bootRes2(3,jdx) = mean([ccc(4,3) ccc(5,3) ccc(5,4)]) ;
+    
+    
+end
+end
+
+histogram(bootRes2(1,:)-bootRes2(2,:))
+
 
 
